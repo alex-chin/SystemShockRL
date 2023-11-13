d97b9b5977de04ee0080a8491cd6321c978e64af
Алекс <k142@mail.ru>
2023-11-13 23:18:28 +0300

    part 5 correct

--- entity.py
+++ entity.py
@@ -40,7 +40,7 @@ class Entity:
         return clone
 
 
-def move(self, dx: int, dy: int) -> None:
-    # Move the entity by a given amount
-    self.x += dx
-    self.y += dy
+    def move(self, dx: int, dy: int) -> None:
+        # Move the entity by a given amount
+        self.x += dx
+        self.y += dy
